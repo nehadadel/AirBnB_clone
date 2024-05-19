72d70ae292b68a88305608226400875a09726c26
nehadadel <eng.nehadadel@gmail.com>
2024-05-19 20:40:18 +0300

    update

--- models/base_model.py
+++ models/base_model.py
@@ -36,7 +36,7 @@ class BaseModel:
         should print: [<class name>] (<self.id>) <self.__dict__>
         """
         class_name = self.__class__.__name__
-        return ("[{}]({}){}".format(class_name, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
     def save(self):
         """
